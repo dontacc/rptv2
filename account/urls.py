bdf2ea305ae9bbee2a33b990887cdeeab269728f
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-06 16:28:46 +0330

    Delete account directory

--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from account import views
-from rest_framework.routers import DefaultRouter
-
-
-router=DefaultRouter()
-router.register('customer', views.CustomerViewSet)
-router.register('order', views.OrderViewSet,basename='order')
-# router.register('token/', views.ExampleAuthentication,basename='token')
-urlpatterns =router.urls
