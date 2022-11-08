6a1b77f92952b7951220148fd87475e94de7c005
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-08 13:19:24 +0330

    Delete message directory

--- message/signals.py
+++
@@ -1,9 +0,0 @@
-from core.models import User
-from .models import Score
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save,sender=User)
-def post_save_generate_code(sender,instance,created,*args,**kwargs):
-    if created:
-        Score.objects.create(user=instance)
--- message/views.py
+++
@@ -1,6 +0,0 @@
-from dj_rest_auth.views import PasswordResetConfirmView,PasswordResetView
-from .Serializer import PasswordSerializer
-
-
-class Password(PasswordResetView):
-    serializer_class=PasswordSerializer
