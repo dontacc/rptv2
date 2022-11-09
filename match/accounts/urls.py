e81dd90d6bb2f769ada32d27bc066d555c064ca5
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-09 20:13:38 +0330

    Delete match/accounts directory

--- match/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from accounts import views
-from rest_framework.routers import DefaultRouter
-
-
-router=DefaultRouter()
-router.register('customer', views.CustomerViewSet)
-router.register('order', views.OrderViewSet,basename='order')
-urlpatterns =router.urls
