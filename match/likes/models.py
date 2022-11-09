51d957a6e5726983598a0d8c8cf9a06b00fec8ee
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-09 20:13:59 +0330

    Delete match/likes directory

--- match/likes/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-class Likes(models.Model):
-    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    object_id=models.PositiveIntegerField()
-    class Meta:
-        verbose_name='like'
-        verbose_name_plural='likes'
