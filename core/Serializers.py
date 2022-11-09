2824aa27bb0c12704622f640a9dd2120b414ef6a
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-09 22:56:14 +0330

    Add files via upload

--- core/Serializers.py
+++ core/Serializers.py
@@ -3,10 +3,7 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 from allauth.account.adapter import DefaultAccountAdapter
 from .models import *
 
-# class CustomRegisterSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=User
-#         fields= ['id', 'username', 'email', 'phone', 'id_card']
+
 class CustomRegisterSerializer(RegisterSerializer):
     phone = serializers.CharField()
     id_card = serializers.CharField(max_length=10, validators=[RegexValidator(r'^\d{10}$')])
@@ -15,19 +12,19 @@ class CustomRegisterSerializer(RegisterSerializer):
         instance = {
             'phone': self.validated_data.get('phone', ''),
             'id_card': self.validated_data.get('id_card', ''),
+            'username': self.validated_data.get('username', ''),
         }
-
         return instance
-
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
 
-    def save_user(self, request, user, form, commit=True):
+    def save_user(self, request, user, form, commit=False):
         user = super().save_user(request, user, form, commit)
         data = form.cleaned_data
         user.phone = data.get('phone')
         user.id_card = data.get('id_card')
+        user.username = data.get('username')
         user.save()
         return user
 
