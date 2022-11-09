5a871fadba12ae0b2b7c967edb6e4a27719a9330
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-10 02:06:56 +0330

    Update settings.py

--- rptv2/settings.py
+++ rptv2/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'likes',
 
 ]
-SITE_ID = 1
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -93,16 +93,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rptv2.wsgi.application'
 
-#
-# AUTHENTICATION_BACKENDS = [
-#     # Needed to login by username in Django admin, regardless of `allauth`
-#     'django.contrib.auth.backends.ModelBackend',
-#
-#     # `allauth` specific authentication methods, such as login by e-mail
-#     'allauth.account.auth_backends.AuthenticationBackend',
-# ]
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -144,8 +134,7 @@ STATIC_ROOT=BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
