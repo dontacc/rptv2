cc7cd7bcd9b3dbe77d20ac0196f76ee8d973f343
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-12 20:12:25 +0330

    admin translate

--- core/models.py
+++ core/models.py
@@ -4,23 +4,26 @@ from django.core.validators import RegexValidator
 import uuid
 
 class User(AbstractUser):
-    email = models.EmailField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True,verbose_name='ایمیل')
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    id_card = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{10}$')],unique=True)
-    is_active = models.BooleanField(default=True)
+    id_card = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{10}$')],unique=True,verbose_name='شماره ملی')
+    is_active = models.BooleanField(default=True,verbose_name='فعال بودن ')
     phone_regex = RegexValidator(regex=r'^(\+98?)?{?(0?9[0-9]{9,9}}?)$')
-    phone = models.CharField(validators=[phone_regex], max_length=11, unique=True)
+    phone = models.CharField(validators=[phone_regex], max_length=11, unique=True,verbose_name='شماره مبایل')
     REQUIRED_FIELDS = [ 'email', 'id_card','phone']
 
     class Meta:
-        verbose_name='user'
-        verbose_name_plural='users'
+        verbose_name='کابر'
+        verbose_name_plural='کاربرها'
 
 class password_reset_token_created(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    number = models.CharField(max_length=4, null=True, blank=True)
-    score=models.TimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,verbose_name='کاربر')
+    number = models.CharField(max_length=4, null=True, blank=True,verbose_name='کد')
+    score=models.TimeField(auto_now_add=True,verbose_name='زمان ارسال')
 
     def __str__(self):
         return self.number
+    class Meta:
+        verbose_name='کد بازیابی پسورد'
+        verbose_name_plural='کدهای بازیابی پسورد'
 
