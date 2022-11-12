7728f3f8b0e9902070a43e46681c9b5cbf0d4f6b
niloofar <86254611+niluws@users.noreply.github.com>
2022-11-13 00:45:24 +0330

    admin translate

--- useful_links/models.py
+++ useful_links/models.py
@@ -8,15 +8,24 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.title
+    class Meta:
+        verbose_name='تماس با ما'
+        verbose_name_plural='پیغام ها'
 
 
 class PrivacyPolicy(models.Model):
     explanation = models.TextField(max_length=500)
+    class Meta:
+        verbose_name='پرسش و پایخ و پشتیبانی'
 
 
 class RefundPolicy(models.Model):
     explanation = models.TextField(max_length=500)
+    class Meta:
+        verbose_name='سیاست حفظ حریم خصوصی'
 
 
 class QandASupport(models.Model):
     explanation = models.TextField(max_length=500)
+    class Meta:
+        verbose_name='پرسش و پاسخ ها'
